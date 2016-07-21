e15598f03c34560710053a950eb6270005f201e8
黄 家金 <hjj245148>
2016-07-21 20:17:04 +0800

    all

---
+++ NewsDemo-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsDemo-swift
+//
+//  Created by 黄 家金 on 16/7/21.
+//  Copyright © 2016年 黄 家金. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
